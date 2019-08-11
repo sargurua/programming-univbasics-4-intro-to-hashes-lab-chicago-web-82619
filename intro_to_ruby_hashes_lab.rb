1cdb60c32e91cf8245ed7d9c3a4eeae24cfdf11b
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-11 01:53:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
-  return hash = {:id => 1}
+  return hash = {:id => 100}
 end
 
 def my_hash_creator(key, value)
